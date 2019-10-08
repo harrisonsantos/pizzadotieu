a1afacf0f85612a11914193dc352be64f1793269
JessicaFernandes <jessicaboatto@gmail.com>
2019-10-08 15:12:31 -0300

    Criado os inputs do programa

---
+++ desafio4.py
@@ -0,0 +1,16 @@
+n = input("Numero de clientes: ")
+n = int(n)
+
+if n < 1:
+    exit()
+
+clients = []
+
+for i in range(n):
+    inp = input()
+    try:
+        slipted = inp.split(" ")
+        clients.append([int(slipted[0]), int(slipted[1])])
+    except Exception as identifier:
+        clients.append([i,0])
+
